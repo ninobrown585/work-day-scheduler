47d3cbe531e12fa9b84536ec60a78c8e1dc0841f
Nino Brown <ninobrown58518@gmail.com>
2022-09-01 20:44:34 -0500

    new js code

--- Develop/style.js
+++ Develop/style.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    $("#9am").val(localStorage.getItem("task9"));
-    $("#10am").val(localStorage.getItem("task10"));
-    $("#11am").val(localStorage.getItem("task11"));
-    $("#12am").val(localStorage.getItem("task12"));
-    $("#1pm").val(localStorage.getItem("task1"));
-    $("#2pm").val(localStorage.getItem("task2"));
-    $("#3pm").val(localStorage.getItem("task3"));
-    $("#4pm").val(localStorage.getItem("task4"));
-    $("#5pm").val(localStorage.getItem("task5"));
+    $(".9am").val(localStorage.getItem("task9"));
+    $(".10am").val(localStorage.getItem("task10"));
+    $(".11am").val(localStorage.getItem("task11"));
+    $(".12pm").val(localStorage.getItem("task12"));
+    $(".1pm").val(localStorage.getItem("task1"));
+    $(".2pm").val(localStorage.getItem("task2"));
+    $(".3pm").val(localStorage.getItem("task3"));
+    $(".4pm").val(localStorage.getItem("task4"));
+    $(".5pm").val(localStorage.getItem("task5"));
 })
 
 // current day timestamp
@@ -18,7 +18,7 @@ $("#currentDay").text(currentDay);
 var currentTime = moment().hour();
 
 // Changing colors based on time of day
-$("#time").each(function() {
+$(".time").each(function() {
     var currentElement = $(this).attr("id");
     if(currentTime > currentElement) {
         $(this).children().eq(1).addClass("past");
@@ -27,4 +27,60 @@ $("#time").each(function() {
     } else {
         $(this).children().eq(1).addClass("future");
     }
+})
+
+// global variables
+var task9 = $(".9am");
+var task10 = $(".10am");
+var task11 = $(".11am");
+var task12 = $(".12pm");
+var task1 = $(".1pm");
+var task2 = $(".2pm");
+var task3 = $(".3pm");
+var task4 = $(".4pm");
+var task5 = $(".5pm");
+
+// Jquery click event save user input to local storage, then gets it out of storage and writes it to the text area
+$("#save").click(function(){
+        var input9 = task9.val().trim();
+        localStorage.setItem("task9", input9);
+        $(".9am").val(localStorage.getItem("task9"));
+
+        var input10 = task10.val().trim();
+        localStorage.setItem("task10", input10);
+        $(".10am").val(localStorage.getItem("task10"));
+
+        var input11 = task11.val().trim();
+        localStorage.setItem("task11", input11);
+        $(".11am").val(localStorage.getItem("task11"));
+
+        var input12 = task12.val().trim();
+        localStorage.setItem("task12", input12);
+        $(".12pm").val(localStorage.getItem("task12"));
+
+        var input1 = task1.val().trim();
+        localStorage.setItem("task1", input1);
+        $(".1pm").val(localStorage.getItem("task1"));
+
+        var input2 = task2.val().trim();
+        localStorage.setItem("task2", input2);
+        $(".2pm").val(localStorage.getItem("task2"));
+
+        var input3 = task3.val().trim();
+        localStorage.setItem("task3", input3);
+        $(".3pm").val(localStorage.getItem("task3"));
+
+        var input4 = task4.val().trim();
+        localStorage.setItem("task4", input4);
+        $(".4pm").val(localStorage.getItem("task4"));
+
+        var input5 = task5.val().trim();
+        localStorage.setItem("task5", input5);
+        $("#5pm").val(localStorage.getItem("task5"));
+})
+
+// // clears the whole schedule if user wants to start over
+$("#clear").click(function() {
+    localStorage.clear();
+    location.reload();
 })
